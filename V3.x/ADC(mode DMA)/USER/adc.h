9a707f050a0f271c121d5d5e1b23b1ef72bbb35b
vcheung <sun-be@hotmail.com>
2013-11-03 22:22:20 +0800

    set project ADC(mode DMA)
    * copy from Template
    * copy file usart1.c&usart1.h from project USART

---
+++ V3.x/ADC(mode DMA)/USER/adc.h
@@ -0,0 +1,4 @@
+#ifndef __ADC_H
+#define __ADC_H
+
+#endif /* __ADC_H */
---
+++ V3.x/ADC(mode DMA)/USER/main.c
@@ -0,0 +1,21 @@
+/******************** (C) COPYRIGHT 2012 WildFire Team **************************
+ * 文件名  ：main.c
+ * 描述    ：串口1(USART1)向电脑的超级终端以1s为间隔打印当前ADC1的转换电压值         
+ * 实验平台：stm32f103c8t6
+ * 库版本  ：ST3.5.0
+ *
+ * 作者    ：zhangwj 
+**********************************************************************************/
+#include "stm32f10x.h"
+#include "usart1.h"
+#include "adc.h"
+
+int main(void)
+{
+
+	while (1);
+
+}
+
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
