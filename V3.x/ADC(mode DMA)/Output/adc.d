3aed04bdf03fc9e39da2d48f0fda64911ec74a82
vcheung <sun-be@hotmail.com>
2013-11-04 15:25:08 +0800

    #ADC(mode DMA) project#
    * build successed
    * 外设ADC1起始地址及ADC规则数据寄存器(ADC_DR)的地址偏移均查《RM0008 STM32F10x参考手册》
    * ADC_DR 数据寄存器保存了 ADC 转换后的数值，以它作为 DMA 的传输源地址。使用 DMA1 的通道 1，数据从
    ADC外设的数据寄存器(ADC1_DR_Address)转移到内存(ADC_ConvertedValue 变量)，使用 DMA 循环传输模式。

---
+++ V3.x/ADC(mode DMA)/Output/adc.d
@@ -0,0 +1,13 @@
+..\Output\adc.o: adc.c
+..\Output\adc.o: adc.h
+..\Output\adc.o: ..\CMSIS\stm32f10x.h
+..\Output\adc.o: ..\CMSIS\core_cm3.h
+..\Output\adc.o: C:\Keil\ARM\RV31\INC\stdint.h
+..\Output\adc.o: ..\CMSIS\system_stm32f10x.h
+..\Output\adc.o: ..\USER\stm32f10x_conf.h
+..\Output\adc.o: ..\FWlib\inc\stm32f10x_adc.h
+..\Output\adc.o: ..\CMSIS\stm32f10x.h
+..\Output\adc.o: ..\FWlib\inc\stm32f10x_dma.h
+..\Output\adc.o: ..\FWlib\inc\stm32f10x_gpio.h
+..\Output\adc.o: ..\FWlib\inc\stm32f10x_rcc.h
+..\Output\adc.o: ..\FWlib\inc\stm32f10x_usart.h
--- V3.x/ADC(mode DMA)/USER/main.c
+++ V3.x/ADC(mode DMA)/USER/main.c
@@ -1,6 +1,6 @@
 /******************** (C) COPYRIGHT 2012 WildFire Team **************************
  * 文件名  ：main.c
- * 描述    ：串口1(USART1)向电脑的超级终端以1s为间隔打印当前ADC1的转换电压值         
+ * 描述    ：串口1(USART1)向电脑的超级终端以1s为间隔（软件延时）打印当前ADC1的转换电压值         
  * 实验平台：stm32f103c8t6
  * 库版本  ：ST3.5.0
  *
@@ -10,11 +10,36 @@
 #include "usart1.h"
 #include "adc.h"
 
+// ADC1转换的电压值通过MDA方式传到SRAM
+extern __IO uint16_t ADC_ConvertedValue;
+
+// 局部变量，用于保存转换计算后的电压值			 
+float ADC_ConvertedValueLocal;        
+
+// 软件延时
+void Delay(__IO uint32_t nCount)
+{
+  for(; nCount != 0; nCount--);
+} 
+
+
 int main(void)
 {
+	USART1_Config();
 
-	while (1);
+	ADC1_Init();
 
+	printf("\r\n -------这是一个ADC实验------\r\n");
+	
+	while (1)
+	{
+		ADC_ConvertedValueLocal =(float) ADC_ConvertedValue/4096*3.3; // 读取转换的AD值
+	
+		printf("\r\n The current AD value = 0x%04X \r\n", ADC_ConvertedValue); 
+		printf("\r\n The current AD value = %f V \r\n",ADC_ConvertedValueLocal); 
+
+		Delay(0xffffee);	// 延时 														  
+	}
 }
 
 
